1b206b34ee5ac540314d6fd9d4a83403ac4c51f5
DavidVRLS <57944167+DavidVRLS@users.noreply.github.com>
2023-03-15 16:36:26 +0100

    [Update/ Tag] Rendu final Version 1.0

--- js/index.js
+++ js/index.js
@@ -19,17 +19,61 @@
 
 // Wait for the deviceready event before using any of Cordova's device APIs.
 // See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
-document.addEventListener('deviceready', onDeviceReady, false);
+
+
+function checkConnection() {
+
+
+    var networkState = navigator.connection.type;
+    if (networkState == "none") {
+        alert("Veuillez lancer l'application avec une connection internet pour pouvoir l'utiliser");
+        document.getElementById("num").style.visibility = "hidden";
+        document.getElementById("Titre").style.visibility = "hidden";
+        document.getElementById("envoyer").style.visibility = "hidden";
+    }
+    else {
+        document.getElementById("num").style.visibility = "visible";
+        document.getElementById("Titre").style.visibility = "visible";
+        document.getElementById("envoyer").style.visibility = "visible";
+        return true;
+
+    }
+
+}
+checkConnection();
+//var networkState = navigator.connection.type;
+// var states = {};
+// states[Connection.UNKNOWN]  = 'Unknown connection';
+// states[Connection.ETHERNET] = 'Ethernet connection';
+// states[Connection.WIFI]     = 'WiFi connection';
+// states[Connection.CELL_2G]  = 'Cell 2G connection';
+// states[Connection.CELL_3G]  = 'Cell 3G connection';
+// states[Connection.CELL_4G]  = 'Cell 4G connection';
+// states[Connection.CELL]     = 'Cell generic connection';
+// states[Connection.NONE]     = 'No network connection';
+
+//document.addEventListener("offline", onOffline, false);
+
+// onOffline();
+// function onOffline() {
+//}
+
 document.getElementById("page-content").style.visibility = "hidden";
 var input;
 var numEmetteur;
 var myText;
+
+document.addEventListener('deviceready', onDeviceReady, false);
 function onDeviceReady() {
     // Cordova is now initialized. Have fun!
-
+    var networkState = navigator.connection.type;
     console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
     document.getElementById('deviceready').classList.add('ready');
+    //alert (cordova);
 }
+
+
+
 function showNumberOfTheUser() {
     // document.getElementById("numUilisateur").innerHTML = input;
     //document.write(window.localStorage.getItem("numUser"));
@@ -96,6 +140,10 @@ function handleClick() {
     }
 
 }
+function activeReceptionOfMessage() {
+    getListMessage();
+    setInterval(getListMessage, 500)
+}
 async function getListMessage() {
 
     //let myURL = await fetch("https://munosutu.myhostpoint.ch/chatGetherApi.php");
@@ -104,37 +152,47 @@ async function getListMessage() {
     //document.getElementById("numEmetteur").value = myText[0].Message;
     /* numEmetteur = showNumberOfTheUser();
      console.log(numEmetteur);*/
-     validationRecepteur();
+    validationRecepteur();
     document.getElementById("numEmetteur").value = showNumberOfTheUser();
-    let path = "https://munosutu.myhostpoint.ch/chatGetherApi.php" + "?numRecepteur=" + document.getElementById("numRecepteur").value + "&numEmetteur=" + showNumberOfTheUser();
+    window.localStorage.setItem("numRecepteur", document.getElementById("numRecepteur").value);
+    let path = "https://kegivilo.myhostpoint.ch/chatGetherApi.php" + "?numRecepteur=" + document.getElementById("numRecepteur").value + "&numEmetteur=" + showNumberOfTheUser();
     let myURL = await fetch(path);
     let myResponse = await myURL.text();
-     myText = JSON.parse(myResponse);
+    myText = JSON.parse(myResponse);
     window.localStorage.setItem("message", myText);
     console.log(myText);
     showMessage();
-
-
-
 }
 function showMessage() {
     divWithAllMessage = document.getElementById("allMessage");
     let allMessage = window.localStorage.getItem("message");
-    var message ="";
+    var message = "";
     for (let i = 0; i < myText.length; i++) {
-        if (myText[i]["NumEmetteur"] == window.localStorage.getItem("numUser")) 
-        {
-            message += '<div class="media media-chat media-chat-reverse "><div class="media-body"><p>'+ myText[i]["Message"]+'</p><p class="meta"><time datetime="2018">23:58</time></p></div></div>';
-                 
-            
+        if (myText[i]["NumEmetteur"] == window.localStorage.getItem("numUser")) {
+            message += '<div class="media media-chat media-chat-reverse float-end"><div class="media-body"><p>' + myText[i]["Message"] + '</p></div></div>';
+
+
         }
-        else{
-            message += '<div class="media media-chat"><div class="media-body"><p>'+ myText[i]["Message"]+'</p><p class="meta"><time datetime="2018">23:58</time></p></div></div>';
-             
+        else {
+            message += '<div class="media media-chat"><div class="media-body"><p>' + myText[i]["Message"] + '</p></div></div>';
+
         }
+        message += "<br><br>";
 
     }
     divWithAllMessage.innerHTML = message;
+
+}
+async function sendMessage() {
+
+    let path = "https://kegivilo.myhostpoint.ch/insertMessage.php" + "?numRecepteur=" + window.localStorage.getItem("numRecepteur") + "&numEmetteur=" + showNumberOfTheUser() + "&message=" + document.getElementById("message").value;
+    document.getElementById("message").value = "";
+    let myURL = await fetch(path);
+    let myResponse = await myURL.text();
+    getListMessage();
+    //   var objDiv = document.getElementById("page-content");
+    //   objDiv.scrollTo(0, objDiv.scrollHeight);
+
 
 }
 
